dcf374e65e998cb1530731a8b911274d78e5156e
cseselim <cseselimreza@gmail.com>
2026-04-18 10:47:00 +0600

    CSS Style add

--- resources/views/vendor/backpack/ui/inc/styles.blade.php
+++ resources/views/vendor/backpack/ui/inc/styles.blade.php
@@ -33,3 +33,9 @@
 @if (backpack_theme_config('vite_styles') && count(backpack_theme_config('vite_styles')))
     @vite(backpack_theme_config('vite_styles'))
 @endif
+
+<style>
+    .footer {
+        position: relative;
+    }
+</style>
